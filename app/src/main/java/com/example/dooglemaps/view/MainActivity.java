4c7886eb549af38711b9f5c90bb2adae93fce913
Samuel Osibamowo <samuelosibamowo@fb.com>
2022-06-21 11:35:43 -0700

    Got initial bottom navigation view working with the two main fragments of the app
    Tested using physical samsung device

--- app/src/main/java/com/example/dooglemaps/view/MainActivity.java
+++ app/src/main/java/com/example/dooglemaps/view/MainActivity.java
@@ -1,16 +1,48 @@
 package com.example.dooglemaps.view;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.example.dooglemaps.R;
+import com.example.dooglemaps.fragments.FeedFragment;
+import com.example.dooglemaps.fragments.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
+    final FragmentManager fragmentManager = getSupportFragmentManager();
+    private BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottomNav);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                Fragment fragment;
+                switch (item.getItemId()) {
+                    case R.id.action_feed:
+                        // do something here
+                        fragment = new FeedFragment();
+                        break;
+                    default:
+                        // do something here
+                        fragment = new HomeFragment();
+                        break;
+                }
+                fragmentManager.beginTransaction().replace(R.id.flContainer, fragment).commit();
+                return true;
+            }
+        });
+
     }
 }
